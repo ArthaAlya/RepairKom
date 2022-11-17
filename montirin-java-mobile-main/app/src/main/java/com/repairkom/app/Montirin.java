1b1c5e18edf9d453c0f66bfc60c63ad663d50931
Aldi Maulana <aldimaul07@gmail.com>
2022-11-17 10:05:43 +0700

    menambahkan file isi repairkom.zip

---
+++ montirin-java-mobile-main/app/src/main/java/com/repairkom/app/Montirin.java
@@ -0,0 +1,19 @@
+package com.repairkom.app;
+
+import android.app.Application;
+
+import com.onesignal.OneSignal;
+
+public class Montirin extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
+}
